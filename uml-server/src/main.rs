712778f9694c8cc6ef2bda32b2cb54651aa0226a
sofush <102423577+sofush@users.noreply.github.com>
2025-04-01 16:08:29 +0200

    feat(server): serve static files

--- uml-server/src/main.rs
+++ uml-server/src/main.rs
@@ -1,4 +1,43 @@
-fn main() {
-    let sum = uml_common::add(1, 2);
-    println!("Sum of numbers: {sum}");
+use actix_files::Files;
+use actix_files::NamedFile;
+use actix_web::rt;
+use actix_web::{
+    App, Error, HttpRequest, HttpResponse, HttpServer, Responder, middleware,
+    web,
+};
+use actix_ws::AggregatedMessage;
+use env_logger::Env;
+use futures_util::FutureExt;
+use futures_util::StreamExt;
+use futures_util::pin_mut;
+use state::State;
+use tokio::sync::broadcast::Sender;
+use tokio::sync::broadcast::channel;
+
+mod state;
+
+async fn index() -> impl Responder {
+    NamedFile::open_async("./uml-server/static/index.html")
+        .await
+        .unwrap()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::new());
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(State {}))
+            .service(web::resource("/").to(index))
+            .service(
+                Files::new("/static", "./uml-server/static").prefer_utf8(true),
+            )
+            .service(Files::new("/wasm", "./uml-wasm/wasm").prefer_utf8(true))
+            .wrap(middleware::NormalizePath::trim())
+            .wrap(middleware::Logger::default())
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
