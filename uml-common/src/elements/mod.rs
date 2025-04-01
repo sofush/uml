a2c73771b7f932de1f2dfa64f9d935148f315c2c
sofush <102423577+sofush@users.noreply.github.com>
2025-04-01 16:56:50 +0200

    feat(wasm): add boilerplate code for drawing elements

---
+++ uml-common/src/elements/mod.rs
@@ -0,0 +1,5 @@
+mod element;
+mod rectangle;
+
+pub use element::*;
+pub use rectangle::*;
--- uml-wasm/src/lib.rs
+++ uml-wasm/src/lib.rs
@@ -1,9 +1,14 @@
-use log::{Level, debug};
+use html_canvas::HtmlCanvas;
+use log::Level;
+use uml_common::{draw_context::Canvas, elements::Rectangle};
 use wasm_bindgen::prelude::*;
+
+mod html_canvas;
 
 #[wasm_bindgen(start)]
 fn run() -> Result<(), JsValue> {
     console_log::init_with_level(Level::Debug).unwrap();
-    debug!("Hello, world!");
+    let canvas = HtmlCanvas::new();
+    canvas.draw_rectangle(Rectangle {});
     Ok(())
 }
