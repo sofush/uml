8b9b1dc384bc85055c278849d67c807dbf7f61ba
sofush <102423577+sofush@users.noreply.github.com>
2025-04-08 08:54:08 +0200

    feat(server): remove closed client handlers

--- uml-server/src/id.rs
+++ uml-server/src/id.rs
@@ -1,13 +1,20 @@
+use std::fmt::Display;
 use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering::Relaxed;
 
 static GLOBAL_THREAD_COUNT: AtomicUsize = AtomicUsize::new(1);
 
-#[derive(PartialEq, Eq, Clone, Copy)]
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub struct Id(usize);
 
 impl Default for Id {
     fn default() -> Self {
         Self(GLOBAL_THREAD_COUNT.fetch_add(1, Relaxed))
+    }
+}
+
+impl Display for Id {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{}", self.0))
     }
 }
