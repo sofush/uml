738d8eaf8bf206b490060d5d229608afbd3616f3
sofush <102423577+sofush@users.noreply.github.com>
2025-05-09 15:38:19 +0200

    feat(common): draw dots in the background of canvas

--- uml-common/src/document.rs
+++ uml-common/src/document.rs
@@ -1,7 +1,7 @@
 use crate::{
     camera::Camera,
     canvas::Canvas,
-    color::WHITE,
+    color::{Color, WHITE},
     drawable::Drawable,
     elements::{Element, Rectangle},
 };
@@ -23,6 +23,26 @@ impl Drawable for Document {
             Rectangle::new(i32::MIN, i32::MIN, u32::MAX, u32::MAX, WHITE)
                 .into();
         clear_rect.draw(canvas, camera);
+
+        const SIZE: u32 = 2;
+        const SPACE: f64 = 75.0;
+        let offx = SPACE - (camera.x() % SPACE);
+        let offy = SPACE - (camera.y() % SPACE);
+        let dot_color = Color::Rgb {
+            red: 170,
+            green: 170,
+            blue: 170,
+        };
+
+        for row in -1..100 {
+            for col in -1..100 {
+                let x = (row as f64 * SPACE) + offx;
+                let y = (col as f64 * SPACE) + offy;
+                let rect =
+                    Rectangle::new(x as _, y as _, SIZE, SIZE, dot_color);
+                rect.draw_fixed(canvas);
+            }
+        }
 
         for drawable in &self.drawables {
             drawable.draw(canvas, camera);
